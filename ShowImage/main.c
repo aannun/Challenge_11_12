5d5870f4378a553abd897b9d8f156166ff84356e
Galante Marco <marco.galante1994@gmail.com>
2018-12-11 12:39:32 +0100

    Added window closing (x pressed) event, input by terminal.

--- ShowImage/main.c
+++ ShowImage/main.c
@@ -1,10 +1,24 @@
-#include <utils.h>
+#include "utils.h"
+#include <stdio.h>
 
+static int LoadMedia(char path[])
+{
 
+    SDL_LockSurface(screen);
+    /* load image*/
+    image_to_load = SDL_LoadBMP(path);
+    if (image_to_load == NULL)
+    {
+        SDL_Log("Failed to load image %s ", SDL_GetError());
+        return -1;
+    }
+    SDL_BlitSurface(image_to_load, NULL, screen, NULL);
+    SDL_UnlockSurface(screen);
 
+    return 0;
+}
 
-
-void main(int argc, char const **argv)
+int main(int argc, char const **argv)
 {
     int ret = 0;
 
@@ -15,7 +29,7 @@ void main(int argc, char const **argv)
         goto cleanup;
     }
 
-    SDL_Window *window = SDL_CreateWindow("tetris", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, 0);
+    SDL_Window *window = SDL_CreateWindow("Image loader 3000", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, 0);
     if (!window)
     {
         SDL_Log("unable to create window: %s", SDL_GetError());
@@ -31,23 +45,41 @@ void main(int argc, char const **argv)
         goto cleanup3;
     }
 
+    screen = SDL_GetWindowSurface(window);
 
-    printf("Insert Image Path");
-    char pathString[]= scanf();
+    printf("Insert Image Path \n");
+    char pathString[256];
+    fgets(&pathString,sizeof(pathString),stdin);
 
-   for (;;)
-	{
-        
+    SDL_Log("%s",pathString);
+
+    if (LoadMedia(pathString) == -1)
+    {
+        SDL_Log("Error in loading img");
     }
 
+   if(SDL_UpdateWindowSurface(window) != 0)
+   {
+        SDL_Log("Error in loading img to window %s",SDL_GetError());
+   }
+    for (;;)
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+            {
+                goto cleanup4;
+            }
+        }
+    }
 
-
+cleanup4:
+    SDL_DestroyRenderer(renderer);
 cleanup3:
+    SDL_DestroyWindow(window);
 cleanup2:
-cleanup1:
-cleanup:
-
     SDL_Quit();
-
-    return 0;
+cleanup:
+    return ret;
 }
---
+++ ShowImage/utils.h
@@ -0,0 +1,6 @@
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
+#define IMGPATH  "C:\Users\Marco Galante\Pictures\Claptrap8Bit.BMP"
+
+SDL_Surface *screen;
+SDL_Surface *image_to_load;
